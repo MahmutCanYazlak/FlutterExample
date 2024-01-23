68a617db99be7b8c0f64b23acbbfba4716c2a96d
Mahmut Can Yazlak <canmahmutyazlak@gmail.com>
2024-01-23 17:33:49 +0300

    update

---
+++ lib/config/routes/app_router.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_firs_project/config/routes/route_names.dart';
+import 'package:flutter_firs_project/features/auth/view/login.dart';
+import 'package:flutter_firs_project/features/auth/view/sign_in.dart';
+
+import '../../features/auth/view/sign_up.dart';
+
+class AppRouter {
+  static Route onGenerateRoutes(RouteSettings settings) {
+    switch (settings.name) {
+      case '/':
+        return _materialRoute(
+          const Login(),
+        );
+      case RouteNames.login:
+        return _materialRoute(
+          const Login(),
+        );
+
+      case RouteNames.signIn:
+        return _materialRoute(
+          const SignIn(),
+        );
+      case RouteNames.signUp:
+        return _materialRoute(
+          const SignUp(),
+        );
+
+      default:
+        return _materialRoute(
+          const Login(),
+        );
+    }
+  }
+
+  static Route<dynamic> _materialRoute(Widget view) {
+    return MaterialPageRoute(builder: (_) => view);
+  }
+}
